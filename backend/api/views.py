69f2f5f571a9fe0927e39fd9eb2ee9269b265406
brightonchebor <cheborbrighton805@gmail.com>
2025-03-04 09:27:05 +0300

    cn

--- backend/api/views.py
+++ backend/api/views.py
@@ -12,3 +12,61 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 
 # Create your views here.
+
+User = get_user_model()
+
+class UserCreate(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_class = [AllowAny]
+
+class userDetailView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_class = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+@login_required
+def google_login_callback(request):
+
+    user = request.user
+
+    social_accounts = SocialAccount.objects.filter(user=user)
+    print('Social Account for User: ', social_accounts)
+
+    social_account = social_accounts.first()
+
+    if not social_account:
+        print('No social account for user: ', user)
+        return redirect('http://localhost:5173/login/callback/?error=NoSocialAccount')
+    
+    token = SocialToken.objects.filter(account=social_account, account__providers='google').first()
+
+    if token:
+        print('Google Token Found: ', token.Token)
+        refresh = RefreshToken.for_user(user)
+        access_token = str(refresh.access_token)
+        return redirect(f'http://localhost:5173/login/callback/?access_token={access_token}')
+
+    else:
+        print('No Google Token Found: ', user)
+        return redirect(f'http://localhost:5173/login/callback/?error=NoGoogleToken')
+
+       
+@csrf_exempt
+def validate_google_token(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            google_access_token = data.get('access_token')
+            print(google_access_token)
+
+            if not google_access_token:
+                return JsonResponse({'detail': 'Access Token is missing.'}, status=400)
+            return JsonResponse({'valid': True})
+        except json.JSONDecodeError:
+            return JsonResponse({'detail': 'Invalid JSON.'}, status=400)
+    return JsonResponse({'detail': 'Method not allowed.'}, status=405)
+
