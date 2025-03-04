47f1f18c997c865a23a093fa734f6926e354ff4e
brightonchebor <cheborbrighton805@gmail.com>
2025-03-04 10:45:51 +0300

    cn

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
 import Navbar from "./components/Navbar"
+import NotFound from "./components/NotFound"
 
 function App() {
   
---
+++ frontend/src/components/NotFound.jsx
@@ -0,0 +1,13 @@
+import "../styles/NotFound.css";
+
+function NotFound() {
+
+    return (
+        <div className="notfound">
+            <h1>404: pPage Not Found</h1>
+            <p>Sorry, the page you're looking for doesn't exist.</p>
+        </div>
+    );
+}
+
+export default NotFound;
